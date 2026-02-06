20e95da33efe5bb451e99c7d5ff5d32e88c82a39
Sakibur59 <sakiburrahman5978@gmail.com>
2026-02-06 21:30:34 +0600

    Valid otp

---
+++ problem2.js
@@ -0,0 +1,14 @@
+function validOtp(otp) {
+    if(typeof otp !== "string"){
+        return "Invalid";
+    }
+
+    if(otp.startsWith("ph-") && otp.length === 8){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let output =validOtp("ph-12346");
+console.log(output);
